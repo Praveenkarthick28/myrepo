af71de39f698cb541891b76ef70c98964066fad0
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-06-30 18:54:27 +0530

    Create uppercase.sh

---
+++ bashscripts/uppercase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This is to print the content in upper case
+
+U1=$(tr[:lower:] [:UPPER:] < $1)
+
+echo " printing the content in upper case "
+#########################################################
+echo $U1
+
