bf09d064e53aec119472de8101ea8bf730bf144a
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-06-30 15:56:01 +0530

    Update nproc

--- bashscripts/print_cpu.sh
+++ bashscripts/print_cpu.sh
@@ -2,6 +2,6 @@
 #for printing the number of CPU's in the system
 
 ############################################################
-cpu = $nproc
+cpu =$(nproc)
 
 echo " The number of cpu's in your system is ${cpu} "
