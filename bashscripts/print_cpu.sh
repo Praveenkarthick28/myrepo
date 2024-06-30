8415084a01b5caddd4741e7255ae78ad6c194400
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-06-30 15:00:15 +0530

    Create bashscripts and print_cpu.sh

---
+++ bashscripts/print_cpu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#for printing the number of CPU's in the system
+
+############################################################
+cpu = $nproc
+
+echo " The number of cpu's in your system is ${cpu} "
