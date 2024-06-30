a7ddac88af96a2c563352c58f6417c615e0062ae
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-06-30 16:00:48 +0530

    Create cal.sh to print calender

---
+++ bashscripts/cal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This is to print the calender for the year
+echo " Type the year "
+
+#user input
+read year
+
+echo "printing cal for ${year}"
+
+################################################
+cal ${year}
+
