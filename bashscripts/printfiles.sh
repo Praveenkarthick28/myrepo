604653534fe88ec028d0880e902130420f68d10a
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-07-01 16:32:04 +0530

    Update printfiles.sh

--- bashscripts/printfiles.sh
+++ bashscripts/printfiles.sh
@@ -4,7 +4,8 @@ found=false
 # This is to check and open particular file
 for i in /etc/*; do
   if [ $i = "/etc/magic" ]; then
-    cat $i
+    cat $i >> printed.txt
+#if you want to store in another file    
     found=true
     break
   fi
