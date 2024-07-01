f14366a31e84fcd5b32ca80bc6fc156e3e5013dd
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-07-01 14:28:24 +0530

    Create leapyear.sh

---
+++ bashscripts/leapyear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#This is to find the leap year
+
+year=$1
+if [ $(($year % 400)) -eq 0 ]; then
+    echo "$year is a leap year."
+elif [ $(($year % 4)) -eq 0 ] && [ $(($year % 100)) -ne 0 ]; then
+    echo "$year is a leap year."
+else
+    echo "$year is not a leap year."
+fi
