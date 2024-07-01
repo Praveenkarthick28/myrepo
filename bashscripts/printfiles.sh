5db8667f4547562cf7a6cde024fa9bb8ea9b265e
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-07-01 16:24:34 +0530

    Update printfiles.sh

--- bashscripts/printfiles.sh
+++ bashscripts/printfiles.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-#To print the files in the folder
-#here to print all the file in etc folder
+#here it will open the magic file in etc/magic
+found=false
+# This is to check and open particular file
 for i in /etc/*; do
-  echo $i
+  if [ $i = "/etc/magic" ]; then
+    cat $i
+    found=true
+    break
+  fi
 done
+
+if [ "$found" = false ]; then
+   echo -n " No file found , OOPS!"
+fi
