2db4ecefca4c10bb3d452c986373ab40df36584f
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-07-06 17:56:55 +0530

    Update provider.tf

--- terraformblock/day2-vpc/provider.tf
+++ terraformblock/day2-vpc/provider.tf
@@ -1,5 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+    }
+    vpc = {
+      source = "hashicorp/vpc"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 provider "aws" {
   # Configuration options
   region = "us-east-1"
-  profile ="terraform"
+  profile = "terraform"
 }
