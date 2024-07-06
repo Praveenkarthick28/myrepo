86b533b1973336750ef9805d2c8384584dcf0bba
Praveenkarthick28 <155005347+Praveenkarthick28@users.noreply.github.com>
2024-07-06 16:32:06 +0530

    Update userdata.sh

--- terraformblock/day2-vpc/userdata.sh
+++ terraformblock/day2-vpc/userdata.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">This is server 1</span></h2>
-  <p>Welcome to CloudChamp's Channel</p>
+  <p>vanakkam tamizha</p>
   
 </body>
 </html>
